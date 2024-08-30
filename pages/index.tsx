ebacd8977a9813225a1efa59b4cff972ace7c3a5
S V S Sudheep Rao <sudheeprao@Ss-MacBook-Air.local>
2024-08-30 12:30:33 +0530

    feat: updated home screen text

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,9 +25,10 @@ export default function Home() {
 
   return (
     <div className="flex flex-col justify-center items-center min-h-screen bg-gray-100 font-sans">
-      <h1 className="m-0 text-4xl font-bold text-gray-800 text-center">Welcome to the Bounty Management System</h1>
+      <h1 className="m-0 text-4xl font-bold text-gray-800 text-center">Welcome to the Paisa-baat</h1>
+      <p className="my-4 text-lg text-center text-gray-500">a easy-to-use Bounty Management System</p>
       <p className="my-4 text-lg text-center text-gray-500">Manage and claim bounties on GitHub with ease.</p>
-      <p className="my-4 text-lg text-center text-gray-500">You&apos;ll need to connect a Solana wallet to participate.</p>
+      <p className="my-4 text-lg text-center text-gray-500">You&apos;ll need to connect a Solana wallet to create and claim bounties.</p>
       <button
         className="bg-blue-500 text-white py-4 px-8 rounded-lg cursor-pointer transition duration-200 ease-in-out hover:bg-blue-600"
         onClick={startAuthorization}
